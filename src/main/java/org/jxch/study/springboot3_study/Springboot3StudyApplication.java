116eae7dc0aab4058184bcf5a791e53e12a3c016
jiangxicheng <jiangxicheng@msunhealth.com>
2022-11-20 15:49:55 +0800

    init

---
+++ src/main/java/org/jxch/study/springboot3_study/Springboot3StudyApplication.java
@@ -0,0 +1,13 @@
+package org.jxch.study.springboot3_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jxch/study/springboot3_study/Springboot3StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jxch.study.springboot3_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
